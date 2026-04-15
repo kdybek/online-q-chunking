f599307642063327b615f0ed97eb3cd0f232e7b4
kdybek <kam.dyb1@gmail.com>
2026-04-15 16:48:30 +0200

    updated run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -27,14 +27,40 @@ source $VENV/bin/activate
 
 pip install -e . -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 
+FLAGS="--num_evals 200 \
+  --total_env_steps 60000000 \
+  --batch_size 256 \
+  --num_envs 512 \
+  --discounting 0.99 \
+  --action_repeat 1 \
+  --unroll_length 62 \
+  --min_replay_size 1000 \
+  --max_replay_size 10000 \
+  --contrastive_loss_fn sym_infonce \
+  --energy_fn l2 \
+  --train_step_multiplier 1 \
+  --log_wandb"
+
+ENV=$1
+
+for seed in 0 1 2; do
+    jaxgcrl crl \
+        --env $ENV \
+        --seed $seed \
+        --wandb_group "online_q_chunking" \
+        --exp_name "ant_baseline_crl_seed_${seed}" \
+        $FLAGS
+done
+
 for seed in 0 1 2; do
     for action_chunk_length in 1 3 5; do
-        jaxgcrl crl \
-            --env ant \
+        jaxgcrl accrl \
+            --env $ENV \
             --action_chunk_length $action_chunk_length \
             --seed $seed \
             --wandb_group "online_q_chunking" \
-            --exp_name "ant_acl_${action_chunk_length}_seed_${seed}"
+            --exp_name "ant_acl_${action_chunk_length}_seed_${seed}" \
+            $FLAGS
     done
 done
 
